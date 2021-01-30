aa361de2693d2a6a070a8c1e6681990b39061fde
玖亖伍 <gsw945@foxmail.com>
2021-01-30 16:45:16 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # go_mod_demo
+
+```
+go mod init github.com/gsw945/go_mod_demo
+```
+
+```
+go test
+go run .
+```
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
+import "rsc.io/quote"
+
 func Hello() string {
-    return "Hello, world."
+    // return "Hello, world."
+    return quote.Hello()
 }
 
 
